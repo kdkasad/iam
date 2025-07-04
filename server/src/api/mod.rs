662ed60faecec5b0c3e545a3f1af511b15084b54
Kian Kasad <kian@kasad.com>
2025-07-04 16:37:11 -0700

    feat: add tracing/limiting middleware

--- server/src/api/mod.rs
+++ server/src/api/mod.rs
@@ -1,11 +1,18 @@
 use std::sync::Arc;
 
-use axum::Router;
+use axum::{Router, http::header};
 use tokio::net::TcpListener;
+use tower::ServiceBuilder;
+use tower_http::{
+    limit::RequestBodyLimitLayer, sensitive_headers::SetSensitiveHeadersLayer, trace::TraceLayer,
+};
 
 use crate::db::interface::DatabaseClient;
 
 mod v1;
+
+/// Maximum request payload size in bytes
+const MAX_REQUEST_PAYLOAD_BYTES: usize = 8 * 1024; // 8 KiB
 
 /// IAM API server
 pub struct ApiServer {
@@ -15,8 +22,15 @@ pub struct ApiServer {
 impl ApiServer {
     pub fn new<D: DatabaseClient>(db: D) -> Self {
         let db: Arc<dyn DatabaseClient> = Arc::new(db);
-        let router: Router<()> =
-            Router::new().nest_service("/api/v1", v1::router().with_state(Arc::clone(&db)));
+        let router: Router<()> = Router::new()
+            .nest_service("/api/v1", v1::router().with_state(Arc::clone(&db)))
+            .layer(
+                // order is top to bottom
+                ServiceBuilder::new()
+                    .layer(SetSensitiveHeadersLayer::new(vec![header::AUTHORIZATION]))
+                    .layer(TraceLayer::new_for_http())
+                    .layer(RequestBodyLimitLayer::new(MAX_REQUEST_PAYLOAD_BYTES)),
+            );
 
         Self { router }
     }
