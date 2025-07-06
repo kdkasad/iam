9a6f36c7d88eb94ef8b2094c6a47c168daa310fb
Kian Kasad <kian@kasad.com>
2025-07-06 00:36:17 -0700

    add shadcn-svelte login page template

---
+++ ui/src/lib/components/login-form.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+	import GalleryVerticalEndIcon from "@lucide/svelte/icons/gallery-vertical-end";
+	import type { HTMLAttributes } from "svelte/elements";
+	import { Label } from "$lib/components/ui/label/index.js";
+	import { Input } from "$lib/components/ui/input/index.js";
+	import { Button } from "$lib/components/ui/button/index.js";
+	import { cn, type WithElementRef } from "$lib/utils.js";
+
+	let {
+		ref = $bindable(null),
+		class: className,
+		...restProps
+	}: WithElementRef<HTMLAttributes<HTMLDivElement>> = $props();
+
+	const id = $props.id();
+</script>
+
+<div class={cn("flex flex-col gap-6", className)} bind:this={ref} {...restProps}>
+	<form>
+		<div class="flex flex-col gap-6">
+			<div class="flex flex-col items-center gap-2">
+				<a href="##" class="flex flex-col items-center gap-2 font-medium">
+					<div class="flex size-8 items-center justify-center rounded-md">
+						<GalleryVerticalEndIcon class="size-6" />
+					</div>
+					<span class="sr-only">Acme Inc.</span>
+				</a>
+				<h1 class="text-xl font-bold">Welcome to Acme Inc.</h1>
+				<div class="text-center text-sm">
+					Don&apos;t have an account?
+					<a href="##" class="underline underline-offset-4"> Sign up </a>
+				</div>
+			</div>
+			<div class="flex flex-col gap-6">
+				<div class="grid gap-3">
+					<Label for="email-{id}">Email</Label>
+					<Input id="email-{id}" type="email" placeholder="m@example.com" required />
+				</div>
+				<Button type="submit" class="w-full">Login</Button>
+			</div>
+		</div>
+	</form>
+</div>
